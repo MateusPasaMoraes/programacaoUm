ebb0991580f12f33e8d028499acc232c98d496a0
Mateus Pasa <mateuspasamoraes@gmail.com>
2026-08-11 08:13:18 -0300

    Primeiro commit

---
+++ src/exerciciosUm/Exercicio11.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+public class Exercicio11 {
+
+	public static void main(String[] args) {
+
+		String nomeAluno = "Manuel Campos";
+		int nota = 6;
+		int frequencia = 75;
+
+		String resultado = (nota >= 6 && frequencia >= 75) ? "Aprovado" : "Reprovado";
+
+		System.out.println("Aluno: " + nomeAluno);
+		System.out.println("Resultado: " + resultado);
+
+	}
+
+}
